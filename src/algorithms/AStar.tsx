53bfb79cabe234b6255a82bb38205667b3ebf558
Michael McNair <michaeltsuyoshi@gmail.com>
2023-12-20 12:56:43 +0900

    clear visualization when running again

--- src/algorithms/AStar.tsx
+++ src/algorithms/AStar.tsx
@@ -6,6 +6,10 @@ export default function AStar(
   aStarInterval: any,
   setRunning: any
 ) {
+  let newMap = map.map((row) =>
+    row.map((box) => (box === 'c' || box === 'o' || box === 'p' ? '#' : box))
+  );
+
   const a: any = {};
   const b: any = {};
 
@@ -46,85 +50,86 @@ export default function AStar(
       return current.fCost <= min.fCost ? current : min;
     }, open[0]);
 
-    open = open.filter((box: Box) => box !== current);
-    closed.push(current);
+    if (current) {
+      open = open.filter((box: Box) => box !== current);
+      closed.push(current);
 
-    if (current.x === b.x && current.y === b.y) {
-      clearInterval(aStarInterval);
-      setRunning(false);
+      if (current.x === b.x && current.y === b.y) {
+        clearInterval(aStarInterval);
+        setRunning(false);
 
-      let temp = current;
-      while (temp) {
-        path.unshift(temp); // Add to the beginning of the array
-        temp = temp.parent!; // Traverse back using parent pointers
-      }
-    }
-
-    const neighbour: { x: number; y: number }[] = [];
-
-    for (let x = -1; x < 2; x++) {
-      for (let y = -1; y < 2; y++) {
-        if (x !== 0 || y !== 0) {
-          neighbour.push({ x: current.x + x, y: current.y + y });
+        let temp = current;
+        while (temp) {
+          path.unshift(temp); // Add to the beginning of the array
+          temp = temp.parent!; // Traverse back using parent pointers
         }
       }
-    }
 
-    neighbour.forEach((box: any) => {
-      if (map[box.y] && map[box.y][box.x] && map[box.y][box.x] !== 'w') {
-        if (
-          !closed.some(
-            (closedBox: Box) => closedBox.x === box.x && closedBox.y === box.y
-          )
-        ) {
-          let currentGCost;
+      const neighbour: { x: number; y: number }[] = [];
 
-          if (current.parent) {
-            if (diagonal(current.parent.x, current.parent.y, current)) {
-              currentGCost = current.parent.gCost + 14;
+      for (let x = -1; x < 2; x++) {
+        for (let y = -1; y < 2; y++) {
+          if (x !== 0 || y !== 0) {
+            neighbour.push({ x: current.x + x, y: current.y + y });
+          }
+        }
+      }
+
+      neighbour.forEach((box: any) => {
+        if (map[box.y] && map[box.y][box.x] && map[box.y][box.x] !== 'w') {
+          if (
+            !closed.some(
+              (closedBox: Box) => closedBox.x === box.x && closedBox.y === box.y
+            )
+          ) {
+            let currentGCost;
+
+            if (current.parent) {
+              if (diagonal(current.parent.x, current.parent.y, current)) {
+                currentGCost = current.parent.gCost + 14;
+              } else {
+                currentGCost = current.parent.gCost + 10;
+              }
             } else {
-              currentGCost = current.parent.gCost + 10;
+              currentGCost = 0;
             }
-          } else {
-            currentGCost = 0;
-          }
 
-          let gCost;
-          if (diagonal(box.x, box.y, current)) {
-            gCost = currentGCost + 14;
-          } else {
-            gCost = currentGCost + 10;
-          }
-
-          const hCost = calculateHCost(box.x, box.y, b);
-
-          const fCost = gCost + hCost;
-          const existingOpenBox = open.find(
-            (openBox: any) => openBox.x === box.x && openBox.y === box.y
-          );
-
-          if (existingOpenBox) {
-            if (fCost < existingOpenBox.fCost) {
-              existingOpenBox.gCost = gCost;
-              existingOpenBox.hCost = hCost;
-              existingOpenBox.fCost = fCost;
-              existingOpenBox.parent = current;
+            let gCost;
+            if (diagonal(box.x, box.y, current)) {
+              gCost = currentGCost + 14;
+            } else {
+              gCost = currentGCost + 10;
             }
-          } else {
-            open.push({
-              x: box.x,
-              y: box.y,
-              gCost: gCost,
-              hCost: hCost,
-              fCost: fCost,
-              parent: current,
-            });
+
+            const hCost = calculateHCost(box.x, box.y, b);
+
+            const fCost = gCost + hCost;
+            const existingOpenBox = open.find(
+              (openBox: any) => openBox.x === box.x && openBox.y === box.y
+            );
+
+            if (existingOpenBox) {
+              if (fCost < existingOpenBox.fCost) {
+                existingOpenBox.gCost = gCost;
+                existingOpenBox.hCost = hCost;
+                existingOpenBox.fCost = fCost;
+                existingOpenBox.parent = current;
+              }
+            } else {
+              open.push({
+                x: box.x,
+                y: box.y,
+                gCost: gCost,
+                hCost: hCost,
+                fCost: fCost,
+                parent: current,
+              });
+            }
           }
         }
-      }
-    });
-
-    const visualizationArray = map.map((row: any, y: number) => {
+      });
+    }
+    newMap = newMap.map((row: any, y: number) => {
       return row.map((box: any, x: number) => {
         if (box === 'a' || box === 'b') return box;
         if (path.some((pathBox: any) => pathBox.x === x && pathBox.y === y)) {
@@ -145,6 +150,11 @@ export default function AStar(
       });
     });
 
-    return visualizationArray;
+    if (!current) {
+      clearInterval(aStarInterval);
+      setRunning(false);
+    }
+
+    return newMap;
   };
 }
